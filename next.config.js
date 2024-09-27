aa821125b6ca8101e2e1c337ef1a6992a68ac2a7
Yoon79 <93427321+Yoon79@users.noreply.github.com>
2024-09-27 19:44:00 +0900

    error fix2

--- next.config.js
+++ next.config.js
@@ -1,25 +1,15 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
 
 // module.exports = nextConfig;
 // next.config.js
-// module.exports = {
-//   basePath: '/letters',
-//   assetPrefix: '/letters',
-// };
-
-const isProd = process.env.NODE_ENV === 'production';
-
 module.exports = {
-  basePath: isProd ? '/letters' : '',
-  assetPrefix: isProd ? '/letters/' : '',
-  trailingSlash: true, // Ensure URLs have trailing slashes
-  images: {
-    unoptimized: true, // Disable image optimization for static export
-  },
+  basePath: '/letters',
+  assetPrefix: '/letters/',
 };
+
 
 
 
