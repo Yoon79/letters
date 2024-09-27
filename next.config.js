1032cedbbf7290d13b4108136b21b96b154e7603
Yoon79 <93427321+Yoon79@users.noreply.github.com>
2024-09-27 19:20:36 +0900

    deploy3

--- next.config.js
+++ next.config.js
@@ -1,14 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
+//
+// // module.exports = nextConfig;
+// // next.config.js
+// module.exports = {
+//   basePath: '/Yoon79/letters',
+//   assetPrefix: '/Yoon79/letters',
+// };
+
+// next.config.js
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  basePath: isProd ? '/letters' : '',
+  assetPrefix: isProd ? '/letters' : '',
 };
 
-// module.exports = nextConfig;
-// next.config.js
-module.exports = {
-  basePath: '/Yoon79/letters',
-  assetPrefix: '/Yoon79/letters',
-};
 
 
 // module.exports = {
