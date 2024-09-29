6a3e3a5c1f83b268422e262c8715e36c9e1e97b9
Yoon79 <93427321+Yoon79@users.noreply.github.com>
2024-09-29 21:50:10 +0900

    error fix5

--- next.config.js
+++ next.config.js
@@ -1,27 +1,9 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// };
-//
-// // module.exports = nextConfig;
-// // next.config.js
-// module.exports = {
-//   basePath: '/letters',
-//   assetPrefix: '/letters/',
-// };
-//
-
-const isProd = process.env.NODE_ENV === 'production';
-
-module.exports = {
-  basePath: isProd ? '/letters' : '',
-  assetPrefix: isProd ? '/letters/' : '', // Add trailing slash here
-  trailingSlash: true, // Ensure trailing slashes for all paths
-  images: {
-    unoptimized: true, // Disable image optimization for static export
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 };
 
+module.exports = nextConfig;
 
 // module.exports = {
 //   async rewrites() {
