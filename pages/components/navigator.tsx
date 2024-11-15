16b1d4961645044d817ae334e93745439ba43766
Yoon79 <93427321+Yoon79@users.noreply.github.com>
2024-11-15 11:36:07 +0900

    Update navigator.tsx

--- pages/components/navigator.tsx
+++ pages/components/navigator.tsx
@@ -27,11 +27,11 @@ export default function Nevigator() {
   }, []);
 
   return (
-    <div className="mt-4 pb-2 ml-4 font-mono font-bold md:fixed w-full bg-inherit">
+    <div className="mt-4 pb-2 mx-auto font-mono font-bold md:fixed w-full bg-inherit">
       <Disclosure as="nav">
         {({ open }) => (
           <>
-            <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 ">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
               <div className="relative flex items-center justify-between h-16">
                 {/*<div className="absolute inset-y-0 left-0 flex items-center sm:hidden">*/}
                 {/*  /!* Mobile menu button*!/*/}
@@ -46,7 +46,7 @@ export default function Nevigator() {
                 {/*</div>*/}
                 <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                   <div className="flex-shrink-0 flex items-center">
-                    <div className="pt-8 -ml-10">
+                    <div className="pt-8">
                       <Link href="/">
                         {showFirstImage ? (
                           <Image
