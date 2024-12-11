ce00ff5f1a28285636533bb2d2bb651a423cb310
Yoon79 <93427321+Yoon79@users.noreply.github.com>
2024-12-11 19:54:27 +0900

    푸터 수정

--- pages/aboutus/index.tsx
+++ pages/aboutus/index.tsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 import Nevigator from "../components/navigator";
 import Image from "next/image";
 import Footer from "../components/footer";
-import Typewriter from 'typewriter-effect';
+import Typewriter from "typewriter-effect";
 
 const AboutUs: NextPage = () => {
   return (
     <div>
       <Nevigator />
       <div className="max-w-2xl mx-auto py-10 px-4 grid items-center grid-cols-1 gap-y-4 gap-x-8 sm:px-6 lg:max-w-7xl lg:px-8 lg:grid-cols-2">
-        <div className="lg:ml-56 mt-14 xl:ml-64">
+        <div className="flex justify-center">
           <img
             src="/yellowdancer.png"
             alt="Picture of the author"
@@ -20,20 +20,41 @@ const AboutUs: NextPage = () => {
         </div>
         <div className="flex-col pt-5 py-6">
           <div className="font-mono font-bold text-gray-800 text-xl">
-            LETTERS IN THE BOX
+            오렌지빛을 쥐고
           </div>
-          <div className="font-mono mt-1.5 text-gray-700 font-medium">
-          2024
-          </div>
+          <div className="font-mono mt-1.5 text-gray-700 font-medium">2024</div>
           <div className="flex-col pt-5 py-6 font-mono">
-          <Typewriter
+            <Typewriter
               options={{
-              strings: ["안녕하쎄용","스웨덴세탁소의 교환일기장 입니다."],
-              autoStart: true,
-              loop: true,
+                strings: [
+                  "안녕하쎄용",
+                  "스웨덴세탁소의 교환일기장 입니다.",
+                  "우세정👕 ",
+                  "브이로그 보러가기👇",
+                ],
+                autoStart: true,
+                loop: true,
               }}
             />
           </div>
+          <div className="flex">
+            <div className="mr-1 text-gray-700 font-mono font-medium">
+              instagram{" "}
+            </div>
+            <img
+              src="/instagram.png"
+              alt="Picture of the author"
+              width={23}
+              height={20}
+            />
+          </div>
+          <a
+            href="https://www.instagram.com/swedenlaundry_s2/"
+            target="__blank">
+            <div className="text-gray-800 font-semibold font-mono text-l cursor-pointer hover:text-[#e98143]">
+              @swedenlaundry_s2
+            </div>
+          </a>
         </div>
       </div>
       <Footer />
--- pages/components/footer.tsx
+++ pages/components/footer.tsx
@@ -18,15 +18,20 @@ export default function Footer() {
         <div className="text-center pb-2">
           <span className="font-mono text-sm text-[#e98143] sm:text-center dark:text-[#e98143]">
             © 2024{" "}
-            <a
+            {/* <a
               className="hover:underline font-mono cursor-pointer"
               onClick={scrollToTop}>
               스웨덴세탁소 교환일기장
+            </a> */}
+            <a
+              className="hover:underline font-mono cursor-pointer"
+              onClick={scrollToTop}>
+              <Link href="/">스웨덴세탁소 교환일기장</Link>
             </a>
             . All Rights Reserved.
           </span>
         </div>
-        {/* <div>
+        <div>
           <ul className="justify-center flex flex-wrap items-center mt-2 mb-2 text-sm text-[#e98143] dark:text-[#e98143] sm:mt-0">
             <li>
               <Link href="/aboutus">
@@ -35,15 +40,15 @@ export default function Footer() {
                 </p>
               </Link>
             </li>
-            <li>
+            {/* <li>
               <Link href="/contact">
                 <p className="font-mono hover:underline cursor-pointer">
                   contact
                 </p>
               </Link>
-            </li>
+            </li> */}
           </ul>
-        </div> */}
+        </div>
       </div>
     </footer>
   );
